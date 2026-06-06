9d79e7ffff82c989759a47626f55cae044acc28e
DocrateSHOVAN17 <muthuprajan17@gmail.com>
2026-06-06 11:49:43 +0530

    Add files via upload

---
+++ PatternPrinters4.java
@@ -0,0 +1,20 @@
+public class PatternPrinters4 {
+    public static void main(String[] args) {
+        int rows = 4;
+
+        for (int i = 0; i < rows; i++) {
+
+            
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+
+            
+            for (int k = 1; k <= 4; k++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
